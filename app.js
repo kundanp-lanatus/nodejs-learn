6dde2d7d270a5598cf23e2b84ee337c4a1cc69a7
kundanp-lanatus <kundan.p@lanatussystems.com>
2024-10-30 11:57:45 +0530

    my commit

---
+++ app.js
@@ -0,0 +1,11 @@
+require('./test1');
+
+
+const sum = require('./test2');
+
+const a = 10;
+const b = 20;
+
+
+console.log("hello Node");
+console.log(sum(a, b));
---
+++ test2.js
@@ -0,0 +1,8 @@
+console.log("executed test2");
+
+
+function sum(a, b) {
+    return a + b
+}
+
+module.exports = sum;
